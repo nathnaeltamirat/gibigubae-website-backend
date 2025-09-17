4a0d47570c40de3f61449cfbae1b9ee4777ef01f
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-09-17 02:03:40 -0900

    edit some information

--- src/routes/enrollment.route.ts
+++ src/routes/enrollment.route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { enrollStudent, removeEnrollment, getEnrollments } from "../controllers/enrollment.controller.js";
+import { removeEnrollment, getEnrollments } from "../controllers/enrollment.controller.js";
 import { authenticate } from "../middlewares/authenticator.middleware.js";
 
 const enrollmentRouter = Router();
 
-enrollmentRouter.post("/", authenticate, enrollStudent);
+// enrollmentRouter.post("/", authenticate, enrollStudent);
 enrollmentRouter.delete("/:id", authenticate, removeEnrollment);
 enrollmentRouter.get("/", authenticate, getEnrollments);
 
